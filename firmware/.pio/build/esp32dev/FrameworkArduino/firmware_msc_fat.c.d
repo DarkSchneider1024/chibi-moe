25232400ba14b3f7b531e1a04b7a0829ec562a2d
Carrot <carrot771024@gmail.com>
2026-06-13 00:33:14 +0800

    fix: Add contents:write permission to workflow for Github Release

---
+++ firmware/.pio/build/esp32dev/FrameworkArduino/firmware_msc_fat.c.d
@@ -0,0 +1,3 @@
+.pio/build/esp32dev/FrameworkArduino/firmware_msc_fat.c.o: \
+ C:/Users/gueiw/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.c \
+ C:/Users/gueiw/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.h
---
+++ firmware/.pio/build/esp32dev/liba5e/WebSockets/libb64/cdecode.c.d
@@ -0,0 +1,2 @@
+.pio/build/esp32dev/liba5e/WebSockets/libb64/cdecode.c.o: \
+ .pio/libdeps/esp32dev/WebSockets/src/libb64/cdecode.c
